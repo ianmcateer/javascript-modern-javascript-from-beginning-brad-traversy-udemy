ca11ded4fe6659f0df0fce7d60838fad4af402ad
Ian McAteer <ian-mcateer55@hotmail.com>
2019-05-16 09:32:54 +1000

    changes

--- 05-object-orientated-programming/01-constructor-this-keyword.js
+++ 05-object-orientated-programming/01-constructor-this-keyword.js
@@ -1,7 +1,11 @@
+//constructor and the this keyword are important
 // es6 classes syntactic sugar or convenience over constructor functions
 // object literals fine for one case but when multiple objects good to
 // use constructor - also when prototypes inheritance
 
+//object literal eg const brad = {age: 27}
+
+//constructor function - should start with capital
 function Person(name, dob) {
 	this.name = name;
 	this.age = age;
@@ -13,10 +17,11 @@ function Person(name, dob) {
   }
 }
 
+//new keyword instantiates an object
 const brad = new Person("Brad",'9-10-1981');
 const john = new Person("John", '9-11-1992');
 
 console.log(brad);
 
-// in this casethe this keyword refers to the current instance of the object
+// in this case the this keyword refers to the current instance of the object
 // console log this in global scope gets window object
