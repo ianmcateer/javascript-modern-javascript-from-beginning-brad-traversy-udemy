eb9f70e8e0bf8b28379642b4405d6a00b0aba6ce
Ian McAteer <ian-mcateer55@hotmail.com>
2019-05-16 13:56:58 +1000

    changes

--- 05-object-orientated-programming/06-es6-classes.js
+++ 05-object-orientated-programming/06-es6-classes.js
@@ -5,6 +5,7 @@ class Person {
     this.birthday = new Date(dob);
   }
 
+  //these methods are attached to Person.prototype
   greeting() {
     return `Hello there ${this.firstName} ${this.lastName}`;
   }
@@ -22,6 +23,7 @@ class Person {
   // Static properties are only available on the class and not instances of a class. 
   // You might question why this is useful. But the use of static methods can be useful 
   // for things such as utility functions or references.
+  //if not using this.soemthing might be a good case for a static method
   static addNumbers(x, y) {
     return x + y;
   }
